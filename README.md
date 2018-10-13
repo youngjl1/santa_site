2b6fecf727639f6d8d25aa7f4667fbc6edda0270
Luke Young <jlyoung@fb.com>
2018-10-13 14:10:25 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Run
+
+```sh
+$ pip install flask
+$ export FLASK_APP=main.py
+$ flask run
+```
---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/doc/<int:doc_id>')
+def show_user_profile(doc_id):
+    return render_template('doc.html', doc_id=doc_id)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join("./uploads/", filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploaded_file/<filename>')
+def uploaded_file(filename):
+    return "congrats"
